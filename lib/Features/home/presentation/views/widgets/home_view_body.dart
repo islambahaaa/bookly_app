12a3e4e4773b272259499b698a7d59c3f544752e
islambahaaa <bossislam55@gmail.com>
2024-07-07 01:29:54 +0300

    add best seller list view item to home view

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featured_books_list_view.dart';
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -21,6 +22,34 @@ class HomeViewBody extends StatelessWidget {
             style: Styles.titleMedium,
           ),
         ),
+        BestSellerListViewItem(),
+      ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 125,
+          child: AspectRatio(
+            aspectRatio: 2.6 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
