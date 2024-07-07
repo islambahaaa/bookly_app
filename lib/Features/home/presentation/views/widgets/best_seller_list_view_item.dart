f6410484c6709a49d950188f991342c550ca8ef9
islambahaaa <bossislam55@gmail.com>
2024-07-07 04:51:53 +0300

    best seller item done

--- lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,7 +1,9 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
@@ -29,40 +31,44 @@ class BestSellerListViewItem extends StatelessWidget {
             const SizedBox(
               width: 30,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    'Harry Potter and the Goblet of Fire',
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: Styles.textStyle20.copyWith(
-                      fontFamily: kGtSectraFine,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire',
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: Styles.textStyle20.copyWith(
+                        fontFamily: kGtSectraFine,
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                const Text(
-                  'J.K. Rowling',
-                  style: Styles.textStyle14,
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '19.99 €',
-                      style: Styles.textStyle20
-                          .copyWith(fontWeight: FontWeight.bold),
-                    )
-                  ],
-                )
-              ],
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  const Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle14,
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '19.99 €',
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      const BookRating(),
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
---
+++ lib/Features/home/presentation/views/widgets/book_rating.dart
@@ -0,0 +1,29 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BookRating extends StatelessWidget {
+  const BookRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(FontAwesomeIcons.solidStar,
+            size: 15, color: Colors.yellow.shade700),
+        const SizedBox(width: 6.3),
+        const Text(
+          '4.8',
+          style: Styles.textStyle16,
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        Text(
+          '(2458)',
+          style: Styles.textStyle14.copyWith(color: const Color(0xFF707070)),
+        ),
+      ],
+    );
+  }
+}
