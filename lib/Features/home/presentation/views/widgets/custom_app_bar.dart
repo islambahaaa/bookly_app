f3d5e3555ce0944b0d7ae594bc43cf47867e7bf2
islambahaaa <bossislam55@gmail.com>
2024-07-08 03:25:07 +0300

    create book details app bar

--- lib/Features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/Features/home/presentation/views/widgets/custom_app_bar.dart
@@ -8,7 +8,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 30, right: 30, top: 42, bottom: 20),
+      padding: const EdgeInsets.only(left: 30, right: 30, top: 12, bottom: 20),
       child: Row(
         children: [
           Image.asset(AssetsData.logo, height: 18),
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
+import 'package:bookly_app/Features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/splash/presentation/views/splach_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeRoute = '/homeView';
+  static const kBookDetailsRoute = '/bookDetailsView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -13,6 +15,10 @@ abstract class AppRouter {
       GoRoute(
         path: kHomeRoute,
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kBookDetailsRoute,
+        builder: (context, state) => const BookDetailsView(),
       ),
     ],
   );
