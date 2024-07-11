fccb653e54ee833b85cd462dcc79e305cc6c49b4
islambahaaa <bossislam55@gmail.com>
2024-07-11 23:37:20 +0300

    create server failure from dio error

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,18 +1,39 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  ApiService apiService;
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:programming&orderBy=newest');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+
+      return left(ServerFailure(
+        e.toString(),
+      ));
+    }
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
 }
--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,46 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioException dioerror) {
+    switch (dioerror.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioerror.response!.statusCode!, dioerror.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Connection Cancelled');
+      case DioExceptionType.unknown:
+        if (dioerror.message!.contains('SocketException')) {
+          return ServerFailure('NO INTERNET CONNECTION');
+        }
+        return ServerFailure('Unexpected Error, Please try again!');
+      default:
+        return ServerFailure('Something went wrong. Please try again!');
+    }
+  }
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your request not found. Please try again!');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal Server Error. Please try again!');
+    } else {
+      return ServerFailure('Something went wrong. Please try again!');
+    }
+  }
+}
