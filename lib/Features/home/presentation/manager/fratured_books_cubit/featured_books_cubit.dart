52e0919d021fcc8b9a4d3bd799c4b587ee8f49e0
islambahaaa <bossislam55@gmail.com>
2024-07-13 15:12:02 +0300

    create newest books states

--- lib/Features/home/presentation/manager/fratured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/fratured_books_cubit/featured_books_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
-import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
